1464266913e3e2a7e47a9828c78459159d8d6ba2
SHAMMAS <muhdshammas612@gmail.com>
2025-03-09 14:35:57 +0530

    Modified models.py for the database

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=12)
+    photo = models.ImageField(upload_to='images/')
+    email = models.EmailField(max_length=100)
+    job = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
