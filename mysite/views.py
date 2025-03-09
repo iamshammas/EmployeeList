9a9aa1675c66109057b0b79726a0b60fe1e3fcd7
SHAMMAS <muhdshammas612@gmail.com>
2025-03-09 15:00:57 +0530

    home function added

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from employee.models import Employee
+
+def home(request):
+    data = Employee.objects.all()
+    context = {
+        'data' = data,
+    }
+    return render(request,'home.html',context)
