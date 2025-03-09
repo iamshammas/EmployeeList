17d9eaa3d3ca73a98b784e58a00609e2a4980821
SHAMMAS <muhdshammas612@gmail.com>
2025-03-09 15:30:20 +0530

    employee photo not showing error solved

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from mysite import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +25,5 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('employee/',include('employee.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
